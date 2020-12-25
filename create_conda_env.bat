fcaacde01272d621bc1350959c1075703f1bad78
nonsabotage <nonsabotage@gmail.com>
2020-12-25 18:02:01 +0900

    Update create_conda_env.bat

--- create_conda_env.bat
+++ create_conda_env.bat
@@ -1,7 +1,9 @@
 conda create -n cv2 python=3.6
 conda activate cv2
 
+pip install ipykernel
 pip install opencv-python
-conda install -y ipykernel
+pip install matplotlib
+pip install japanize_matplotlib
 conda install -y numpy
 conda install -y scipy
